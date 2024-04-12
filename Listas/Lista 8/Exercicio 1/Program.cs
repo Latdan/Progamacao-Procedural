3a267de659be0ba8103214bf991094b2e9d19fe8
Latdan <latiman2014@gmail.com>
2024-04-12 08:01:17 -0300

    Enviando lista de exercicios 8

---
+++ Listas/Lista 8/Exercicio 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+struct Pizza
+{
+    public int id;
+    public string sabor;
+    public double preco;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pizza minhaPizza;
+        
+        Console.WriteLine("Digite o ID da pizza:");
+        minhaPizza.id = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Digite o sabor da pizza:");
+        minhaPizza.sabor = Console.ReadLine();
+
+        Console.WriteLine("Digite o preço da pizza:");
+        minhaPizza.preco = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("\nDados da pizza:");
+        Console.WriteLine($"ID: {minhaPizza.id}");
+        Console.WriteLine($"Sabor: {minhaPizza.sabor}");
+        Console.WriteLine($"Preço: R${minhaPizza.preco:F2}");
+    }
+}
---
+++ Listas/Lista 8/Exercicio 2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+struct Produto
+{
+    public int id;
+    public string nome;
+    public double preco;
+    public bool disponivelEmEstoque;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Produto[] produtos = new Produto[4];
+
+        for (int contador = 0; contador < produtos.Length; contador++)
+        {
+            Console.WriteLine($"Digite os dados do produto {contador + 1}:");
+            
+            Console.Write("ID: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            
+            Console.Write("Preço: ");
+            double preco = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Disponível em estoque (true/false): ");
+            bool disponivelEmEstoque = Convert.ToBoolean(Console.ReadLine());
+            
+            produtos[contador] = new Produto { id = id, nome = nome, preco = preco, disponivelEmEstoque = disponivelEmEstoque };
+        }
+
+        Console.WriteLine("Produtos disponíveis em estoque:");
+        for (int contador = 0; contador < produtos.Length; contador++)
+        {
+            if (produtos[contador].disponivelEmEstoque)
+            {
+                Console.WriteLine($"Nome: {produtos[contador].nome}");
+            
+                Console.WriteLine($"Preço: R${produtos[contador].preco}");
+            }
+        }
+    }
+}
